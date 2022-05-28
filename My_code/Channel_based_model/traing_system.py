b7c0a6169e7557f00b94ef9dbbd5c3c23a840c08
Ming <58097041+nemeritz-ming@users.noreply.github.com>
2022-05-28 22:07:44 +0800

    Update traing_system.py
    update

--- My_code/Channel_based_model/traing_system.py
+++ My_code/Channel_based_model/traing_system.py
@@ -46,25 +46,34 @@ class StockTradingEnv(gym.Env):
         return obs
 
     def _take_action(self, actions):
-        # clear your current limit orders
+        mid_price = (self.b1 + self.a1) / 2
+
+        # check your previous limit orders
         if len(self.lob_record.keys()) != 0:
             for set_price in self.lob_record.keys():
-                if self.lob_record[set_price] > 0 and set_price >= self.b1:
+                if self.lob_record[set_price] > 0 and set_price >= self.a1:
                     # execute buy one!
-                    self.balance -= self.b1 * self.lob_record[set_price]
+                    self.balance -= self.a1 * self.lob_record[set_price]
+                    # self.balance += self.a1 * abs(self.lob_record[set_price]) * 0.0001
 
                     self.shares_held += self.lob_record[set_price]
-                    Trading_Records.append(f"{self.a1}, buy , {abs(self.lob_record[set_price])}, {self.shares_held}, {self.balance}")
+
+                    # Trading_Records.append(f"{self.prev_time},{self.a1}, buy , {abs(self.lob_record[set_price])}, {self.shares_held}, {self.balance}")
+                    # print(f'{self.a1}, buy , {self.lob_record[set_price]} , {self.shares_held}, {mid_price}, {self.balance}')
 
                     self.lob_record[set_price] = 0
 
-                elif self.lob_record[set_price] < 0 and set_price <= self.a1:
+
+                elif self.lob_record[set_price] < 0 and set_price <= self.b1:
                     # execute sell one!
-                    self.balance += self.a1 * -self.lob_record[set_price]
+                    self.balance += self.b1 * -self.lob_record[set_price]
+                    # self.balance += self.b1 * abs(self.lob_record[set_price]) * 0.0001
                     self.shares_held -= -self.lob_record[set_price]
-                    Trading_Records.append(f"{self.b1}, sell , {abs(self.lob_record[set_price])}, {self.shares_held}, {self.balance}")
 
+                    # Trading_Records.append(f"{self.prev_time},{self.b1}, sell , {abs(self.lob_record[set_price])}, {self.shares_held}, {self.balance}")
+                    # print(f'{self.b1}, sell , {self.lob_record[set_price]} , {self.shares_held}, {mid_price}, {self.balance}')
                     self.lob_record[set_price] = 0
+
 
         # Set the current price
         mid_price = (self.b1 + self.a1) / 2
@@ -73,29 +82,30 @@ class StockTradingEnv(gym.Env):
             action_type = action[0]
             amount = action[1]
 
-            # Put one limit ask order at price a (buy stock)
+            # Put one limit bid order at price a (buy stock)
             if action_type == 0:
                 # check it is executed?
-                if amount > self.b1:
-                    # print('transaction executed')
-                    self.balance -= self.b1
+                if amount > self.a1:
+                    self.balance -= self.a1
+                    # self.balance += self.a1 * 0.0001
                     self.shares_held += 1
-                    # print(f"{self.b1}, buy , 1 , {self.shares_held}, {self.balance}")
-                    Trading_Records.append(f"{self.prev_time},{self.b1}, buy , 1 , {self.shares_held}, {self.balance}")
-
+                    # Trading_Records.append(f"{self.prev_time},{self.a1}, buy , 1 , {self.shares_held}, {self.balance}")
+                    # print(f'{self.a1}, buy , 1 , {self.shares_held}, {mid_price}, {self.balance}')
 
                 else:
                     self.lob_record[amount] += 1
 
+            # Put one limit ask order at price a (buy stock)
             elif action_type == 1:
                 # check it is executed?
-                if amount < self.a1:
+                if amount < self.b1:
                     # transaction executed
-                    # print('transaction executed')
-                    self.balance += self.a1
+                    self.balance += self.b1
+                    # self.balance += self.a1 * 0.0001
+
                     self.shares_held -= 1
-                    # print(f"{self.a1}, sell , 1 , {self.shares_held}, {self.balance}")
-                    Trading_Records.append(f"{self.prev_time},{self.a1}, sell , 1 , {self.shares_held}, {self.balance}")
+                    # Trading_Records.append(f"{self.prev_time},{self.b1}, sell , 1 , {self.shares_held}, {self.balance}")
+                    # print(f'{self.b1}, sell , -1 , {self.shares_held}, {mid_price}, {self.balance}')
 
                 else:
                     self.lob_record[amount] -= 1
@@ -104,9 +114,8 @@ class StockTradingEnv(gym.Env):
                 # clear your inventory
                 self.balance += self.shares_held * mid_price
                 tx = "sell" if self.shares_held > 0 else "buy"
-                # print(f"{mid_price}, {tx} ,{abs(self.shares_held)}, 0, {self.balance}")
+                print(f'{mid_price}, {tx} , {self.shares_held} , 0, {mid_price}')
                 # self.balance -= abs(self.shares_held) * mid_price * 0.0001
-
                 Trading_Records.append(f"{self.prev_time},{mid_price}, {tx} ,{abs(self.shares_held)}, 0, {self.balance}")
 
                 # transaction fee
@@ -161,11 +170,11 @@ class StockTradingEnv(gym.Env):
             self.b1 = min([a[0] for a in self.trade_record[self.prev_time]["buy"]])
 
         self._take_action(current_action)
-        if self.net_worth <= 0 or self.balance < 0:
+        if self.net_worth <= 0:
             done = True
 
         if done:
-            print(info['inv'])
+            # print(info['inv'])
             self._take_action([[2, 0]])
         profit = self.net_worth - self.INITIAL_ACCOUNT_BALANCE
         reward = self.net_worth - previous_net_worth
@@ -174,7 +183,7 @@ class StockTradingEnv(gym.Env):
         return obs, reward, done, {"profit": profit, "inv": self.shares_held, "net_worth": self.net_worth,
                                    "time": self.prev_time, 'balance': self.balance,
                                    'a1': self.a1,
-                                   'b1': self.b1}
+                                   'b1': self.b1, 'lob':self.lob_record}
 
     def reset(self, new_df=None):
 
@@ -196,22 +205,19 @@ class StockTradingEnv(gym.Env):
 
         return self.step([[3, 0]])
 
-
 if __name__ == '__main__':
     for day in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
                 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
                 "21", "22", "23", "24", "25", "26", "27", "28","29", "30"]:
         url = '/Users/ming/Documents/Project/data/trades/BTCUSDT-trades-2021-11-{0}.csv'.format(day)
+        print('start at:', day, 'Nov.')
         dec_train1 = pd.read_csv(url)
-        print(dec_train1.columns)
+        # print(dec_train1.columns)
         dec_train1.columns = ['trade_id', 'price', 'qty', 'quoteQty', 'time', 'isBuyerMaker']
         dec_train1 = dec_train1.drop(columns='trade_id')
         max_price = dec_train1['price'].max()
         min_price = dec_train1['price'].min()
         data_len = dec_train1.shape[0]
-        print('data size:', data_len)
-        print('min_price:', min_price)
-        print('max_price:', max_price)
         seq_len = 2000
         num = int(data_len / seq_len)
         profit = []
@@ -220,6 +226,7 @@ if __name__ == '__main__':
         my_data_buy = defaultdict(int)
         my_profit = 0
         mid_price = 0
+        n0 = 4
         for i in range(num):
             df1 = dec_train1.iloc[i * seq_len:(i + 1) * seq_len, :]
             print('from', i * seq_len, ' to ', (i + 1) * seq_len, ':')
@@ -257,17 +264,17 @@ if __name__ == '__main__':
                 action_space = [(3, 0)]
 
                 if time > time_e:
-                    num_interval = 20
+                    num_interval = 40
                     range_ = 0.06 * (max_price - min_price)
                     delta = range_ / num_interval
                     buy_ans, sell_ans = plot_bar(my_data_buy, my_data_sell, range_, num_interval, mid_price)
-                    a = buy_ans[buy_ans['col2'] >= buy_ans[buy_ans['col2'] > 0]['col2'].quantile(0.8)]['price'].values
-                    b = sell_ans[sell_ans['col2'] >= sell_ans[sell_ans['col2'] > 0]['col2'].quantile(0.8)][
+                    a = buy_ans[buy_ans['col2'] >= buy_ans[buy_ans['col2'] > 0]['col2'].quantile(0.9)]['price'].values
+                    b = sell_ans[sell_ans['col2'] >= sell_ans[sell_ans['col2'] > 0]['col2'].quantile(0.9)][
                         'price'].values
                     target = np.intersect1d(a, b)
                     for sell_buy_price in target:
-                        if abs(market_price[-1] - sell_buy_price) <= delta and abs(my_inv) <= 3:
-                            action_space = [(0, b1), (1, a1)]
+                        if abs(market_price[-1] - sell_buy_price) <= delta and abs(my_inv) <= 2:
+                            action_space = [(0, sell_buy_price), (1, sell_buy_price)]
                             break
                         else:
                             action_space = [(3, 0)]
@@ -280,10 +287,7 @@ if __name__ == '__main__':
             profit.append(my_profit)
             print(i, 'score for this episode: ', score)
         print('end at:', day)
-        # with open("result_with_fee{0}.txt".format(day), "w") as output:
-        #     output.write(str(profit))
-        # with open("trades_with_fee{0}.txt".format(day), "w") as output:
-        #     output.write(str(Trading_Records))
-        Trading_Records = []
+#         with open("result_{0}.txt".format(day), "w") as output:
+#             output.write(str(profit))
 
 
